2f54c2d3681046870867025c7066c9eabddc3e6d
Bui Doan Quang Huy <77490656+YuhQuang21@users.noreply.github.com>
2021-08-29 23:12:20 +0700

    add type for userlogin

--- DemoTTCSCN/Models/UserLogin.cs
+++ DemoTTCSCN/Models/UserLogin.cs
@@ -11,11 +11,13 @@ namespace DemoTTCSCN.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string IdStudent { get; set; }
+        public int Type { get; set; }
         public UserLogin(DataRow row )
         {
             this.Username = row["UserName"].ToString();
             this.Password = row["Password"].ToString();
             this.IdStudent = row["IdStudent"].ToString();
+            this.Type = Convert.ToInt32(row["Type"]);
         }
         public UserLogin()
         {
